1d09fcf2e97060f5ac862ca2c77236a935f3edae
Ryan Gosine <ryanganeshgosineyarn test exercise-6git config --global user.email ryanganeshgosine>
2024-01-02 17:46:06 -0500

    cleaned up backend

---
+++ server/server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+// const bodyParser = require("body-parser");
+// const { users } = require("./database");
+const {} = require("./handlers");
+
+express()
+  // Below are methods that are included in express(). We chain them for convenience.
+  // --------------------------------------------------------------------------------
+
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan("tiny"))
+  .use(express.json())
+
+  // Any requests for static files will go into the public folder
+  .use(express.static("public"))
+
+  // Nothing to modify above this line
+  //          ENDPOINTS
+  // ---------------------------------
+
+  // ---------------------------------
+  // Nothing to modify below this line
+
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "hmmm... this ain't it.",
+    });
+  })
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(8000, () => console.log(`Listening on port 8000`));
